513e2b20f55efc750ab16d21d94824207c4fdbb1
Jason <jason_skillman@outlook.com>
2020-03-02 11:31:07 -0500

    Basic notification and worker class setup and working

--- app/src/main/java/com/jasoncorp/oneadaycomplement/MainActivity.java
+++ app/src/main/java/com/jasoncorp/oneadaycomplement/MainActivity.java
@@ -1,23 +1,67 @@
 package com.jasoncorp.oneadaycomplement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     public static final String NOTIFICATION_CHANNEL_ID_COMPLEMENT = "channel_complement";
     public static final String NOTIFICATION_CHANNEL_NAME_COMPLEMENT = "Complement";
+    public static final String WORK_TAG = "notificationWork";
+    private String DBEventIDTag = "id_tag";
+    private int DBEventID = 0;
+
+    private Button btnStart;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnStart = findViewById(R.id.btn_start);
+
         setupNotificationChannels();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startWork();
+            }
+        });
+    }
+
+    void startWork() {
+        Log.d(TAG, "startWork: ");
+
+        Data inputData = new Data.Builder().putInt(DBEventIDTag, DBEventID).build();
+
+        OneTimeWorkRequest notificationWork = new OneTimeWorkRequest.Builder(NotificationWorker.class)
+                .setInitialDelay(10, TimeUnit.MILLISECONDS)
+                .setInputData(inputData)
+                .addTag(WORK_TAG)
+                .build();
+
+        WorkManager.getInstance(this).enqueue(notificationWork);
     }
 
     /**
--- app/src/main/java/com/jasoncorp/oneadaycomplement/NotificationWorker.java
+++ app/src/main/java/com/jasoncorp/oneadaycomplement/NotificationWorker.java
@@ -1,15 +1,19 @@
 package com.jasoncorp.oneadaycomplement;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 public class NotificationWorker extends Worker {
 
-    //public static final String CHANNEL_ID = "";
+    private static final String TAG = "NotificationWorker";
+
+    public static final int NOTIFICATION_ID = 0;
 
     private Context context;
 
@@ -22,19 +26,24 @@ public class NotificationWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
+        Log.d(TAG, "doWork: ");
 
+        //Todo: replace with dynamic data
         String title = "Title";
         String text = "text";
 
+        //Build the notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, MainActivity.NOTIFICATION_CHANNEL_ID_COMPLEMENT)
-                //.setSmallIcon(R.drawable.notification_icon)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(title)
                 .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
+        //Start the notification
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+
         return Result.success();
-        // (Returning RETRY tells WorkManager to try this task again
-        // later; FAILURE says not to try again.)
     }
 
 }
