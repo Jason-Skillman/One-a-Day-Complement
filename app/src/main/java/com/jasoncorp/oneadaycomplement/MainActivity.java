6d884d5a341712ad7620efe9c6f3821d534b0781
Jason <jason_skillman@outlook.com>
2020-03-02 16:05:15 -0500

    Added a cancel work button

--- app/src/main/java/com/jasoncorp/oneadaycomplement/MainActivity.java
+++ app/src/main/java/com/jasoncorp/oneadaycomplement/MainActivity.java
@@ -3,15 +3,18 @@ package com.jasoncorp.oneadaycomplement;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.work.Data;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.Operation;
 import androidx.work.WorkManager;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Debug;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,13 +22,11 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    public static final String NOTIFICATION_CHANNEL_ID_COMPLEMENT = "channel_complement";
-    public static final String NOTIFICATION_CHANNEL_NAME_COMPLEMENT = "Complement";
-    public static final String WORK_TAG = "notificationWork";
+    public static final String WORK_TAG_NOTIFICATION = "work_tag_notification";
     private String DBEventIDTag = "id_tag";
     private int DBEventID = 0;
 
-    private Button btnStart;
+    private Button btnStart, btnStop;
 
 
     @Override
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnStart = findViewById(R.id.btn_start);
+        btnStop = findViewById(R.id.btn_stop);
 
         setupNotificationChannels();
     }
@@ -48,6 +50,20 @@ public class MainActivity extends AppCompatActivity {
                 startWork();
             }
         });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelWork();
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        btnStart.setOnClickListener(null);
+        btnStop.setOnClickListener(null);
     }
 
     void startWork() {
@@ -58,10 +74,16 @@ public class MainActivity extends AppCompatActivity {
         OneTimeWorkRequest notificationWork = new OneTimeWorkRequest.Builder(NotificationWorker.class)
                 .setInitialDelay(1000, TimeUnit.MILLISECONDS)
                 .setInputData(inputData)
-                .addTag(WORK_TAG)
+                .addTag(WORK_TAG_NOTIFICATION)
                 .build();
 
         WorkManager.getInstance(this).enqueue(notificationWork);
+    }
+
+    void cancelWork() {
+        Operation operation = WorkManager.getInstance(this).cancelAllWorkByTag(WORK_TAG_NOTIFICATION);
+
+        Toast.makeText(this, "Complements have been turned off", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -69,10 +91,12 @@ public class MainActivity extends AppCompatActivity {
      */
     public void setupNotificationChannels() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String id = NOTIFICATION_CHANNEL_ID_COMPLEMENT;
-            CharSequence name = NOTIFICATION_CHANNEL_NAME_COMPLEMENT;
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            //Set the notification data
+            String id = getString(R.string.notification_channel_id_complement);
+            CharSequence name = getString(R.string.notification_channel_name_complement);
+            int importance = NotificationManager.IMPORTANCE_LOW;
 
+            //Setup the notification channel
             NotificationChannel channel = new NotificationChannel(id, name, importance);
 
             //Register the channel with the system, you can't change the importance or other notification behaviors after this
