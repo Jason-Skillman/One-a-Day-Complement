210ba869df22920c9ed9a4ab150bfe4974758982
Jason <jason_skillman@outlook.com>
2020-03-15 14:20:22 -0400

    Fixed the boot receiver's alarm

--- app/src/main/java/com/jasoncorp/oneadaycomplement/receivers/BootReceiver.java
+++ app/src/main/java/com/jasoncorp/oneadaycomplement/receivers/BootReceiver.java
@@ -7,50 +7,67 @@ import android.content.Context;
 import android.content.Intent;
 import android.icu.util.Calendar;
 
-import com.jasoncorp.oneadaycomplement.MainActivity;
 import com.jasoncorp.oneadaycomplement.UserPreferencesManager;
 
 import java.util.Date;
 
 public class BootReceiver extends BroadcastReceiver {
 
-    private PendingIntent pendingIntent;
     private AlarmManager alarmManager;
-    private int timerHours, timerMinutes;
+    private PendingIntent pendingIntent;
 
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+
             Intent alarmIntent = new Intent(context, AlarmReceiver.class);
             pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);
+
+            //Fetch the time of the alarm
             Date alarmTime = UserPreferencesManager.getInstance().getAlarmTime(context);
-
-            //Update the UI to show the alarm time
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(alarmTime);
-            int hours = calendar.get(Calendar.HOUR_OF_DAY);
-            int minutes = calendar.get(Calendar.MINUTE);
-
-            String hoursStr = "" + hours;
-            if(hours == 0) hoursStr = "" + (hours + 12);
-            else if(hours > 12) hoursStr = "" + (hours - 12);
-            timerHours = Integer.parseInt(hoursStr);
-
-            String minutesStr = "" + minutes;
-            if(minutes < 10) minutesStr += "0";
-            timerMinutes = Integer.parseInt(minutesStr);
+            int hour = getHour(alarmTime);
+            int minute = getMinute(alarmTime);
 
             //Set the time for the alarm
-            calendar.clear();
+            Calendar calendar = Calendar.getInstance();
             calendar.setTimeInMillis(System.currentTimeMillis());
-            calendar.set(Calendar.HOUR_OF_DAY, timerHours);
-            calendar.set(Calendar.MINUTE, timerMinutes);
 
-            //Start the alarm
-            alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+            //Calculate the day of the alarm
+            calendar.clear();
+            if(calendar.get(Calendar.HOUR_OF_DAY) > hour) {   //Hour has already passed
+                calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) + 1);
+            } else if(calendar.get(Calendar.HOUR_OF_DAY) == hour) {   //Hours are the same
+                if(calendar.get(Calendar.MINUTE) > minute) {
+                    calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) + 1);
+                } else {
+                    calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR));
+                }
+            } else {    //Hour is in the future
+                calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR));
+            }
+
+            //Set the hour and minute of the alarm
+            calendar.set(Calendar.HOUR_OF_DAY, hour);
+            calendar.set(Calendar.MINUTE, minute);
+
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
         }
+    }
+
+    private int getHour(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.setTime(date);
+        return calendar.get(Calendar.HOUR_OF_DAY);
+    }
+
+    private int getMinute(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MINUTE);
     }
 
 }
